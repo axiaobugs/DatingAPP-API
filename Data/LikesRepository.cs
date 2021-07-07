6efccf87330c90c5e646f6dbd91c5a4f3d21341a
Yanxiao Fang <axiaobug@hotmail.com>
2021-07-07 10:46:34 +1000

    feat ( (new)  LikesController,LikeEntity,(new) ILikeRepostitory etc) add like feature

---
+++ Data/LikesRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.DTOs;
+using DatingApp.Entities;
+using DatingApp.Extensions;
+using DatingApp.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class LikesRepository:ILikesRepository
+    {
+        private readonly DataContext _contex;
+
+        public LikesRepository(DataContext contex)
+        {
+            _contex = contex;
+        }
+
+
+        public async Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
+        {
+            return await _contex.Likes.FindAsync(sourceUserId, likedUserId);
+        }
+
+        public async Task<AppUser> GetUserWithLikes(int userId)
+        {
+            return await _contex.Users
+                .Include(x => x.LikedUsers)
+                .FirstOrDefaultAsync(x => x.Id == userId);
+        }
+
+        public async Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+        {
+            var users = _contex.Users.OrderBy(u => u.UserName).AsQueryable();
+            var likes = _contex.Likes.AsQueryable();
+
+            if (predicate=="liked")
+            {
+                likes = likes.Where(like => like.SourceUserId == userId);
+                users = likes.Select(like => like.LikedUser);
+            }
+
+            if (predicate == "likedBy")
+            {
+                likes = likes.Where(like => like.LikedUserId == userId);
+                users = likes.Select(like => like.SourceUser);
+            }
+
+            return await users.Select(user => new LikeDto
+            {
+                Username = user.UserName,
+                KnownAs = user.KnownAs,
+                Age = user.DateOfBirth.CalculateAge(),
+                PhotoUrl = user.Photos.FirstOrDefault(p=>p.IsMain).Url,
+                City = user.City,
+                Id = user.Id
+            }).ToListAsync();
+
+        }
+    }
+}
