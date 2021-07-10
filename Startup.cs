4e76197ca5def8a12fb11dfbd91ff5e3c6f2f02d
Yanxiao Fang <axiaobug@hotmail.com>
2021-07-11 09:47:57 +1000

    fix signalR in startUp

--- Startup.cs
+++ Startup.cs
@@ -47,7 +47,10 @@ namespace DatingApp
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("http://localhost:4200"));
 
             app.UseAuthentication();
 
