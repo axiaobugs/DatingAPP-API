01392b0f0a39bcfc8b0acc5cc9d9c509a2e5829e
Yanxiao Fang <axiaobug@hotmail.com>
2021-07-11 11:25:15 +1000

    feat: (MessageHub) Add send message method to the hub

---
+++ SignalR/MessageHub.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatingApp.DTOs;
+using DatingApp.Entities;
+using DatingApp.Extensions;
+using DatingApp.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DatingApp.SignalR
+{
+    public class MessageHub:Hub
+    {
+        private readonly IMessageRepository _messageRepository;
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+
+        public MessageHub(IMessageRepository messageRepository,IMapper mapper,IUserRepository userRepository)
+        {
+            _messageRepository = messageRepository;
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            // get request fetch 'user'
+            var otherUser = httpContext.Request.Query["user"].ToString();
+            var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            var messages = await _messageRepository
+                .GetMessageThread(Context.User.GetUsername(), otherUser);
+            await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(CreateMessageDto createMessageDto)
+        {
+            var username = Context.User.GetUsername();
+            // can't send message to yourself
+            if (username == createMessageDto.RecipientUsername.ToLower())
+                throw new HubException("You can not send message to yourself");
+            // get sender and recipient
+            var sender = await _userRepository.GetUserByUserNameAsync(username);
+            var recipient = await _userRepository.GetUserByUserNameAsync(createMessageDto.RecipientUsername);
+            // if no recipient
+            if (recipient == null)
+                throw new HubException("Not found user");
+            // then create message
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+            // add message to database
+            _messageRepository.AddMessage(message);
+            if (await _messageRepository.SaveAllAsync())
+            {
+                var group = GetGroupName(sender.UserName, recipient.UserName);
+                await Clients.Group(group).SendAsync("NewMessage", _mapper.Map<MessageDto>(message));
+            }
+
+
+        }
+
+        private string GetGroupName(string caller, string other)
+        {
+            var stringCompare = string.CompareOrdinal(caller, other) < 0;
+            return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+        }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -60,6 +60,7 @@ namespace DatingApp
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
+                endpoints.MapHub<MessageHub>("hubs/message");
             });
         }
     }
