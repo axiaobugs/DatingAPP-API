ce8e7d985e4b7efd14ed3d4787ac67d16ede507d
Yanxiao Fang <axiaobug@hotmail.com>
2021-07-11 22:08:45 +1000

    refactor:  dealing with UTC date formats

--- Data/MessageRepository.cs
+++ Data/MessageRepository.cs
@@ -105,7 +105,7 @@ namespace DatingApp.Data
             {
                 foreach (var message in unreadMessages)
                 {
-                    message.DateRead = DateTime.Now;
+                    message.DateRead = DateTime.UtcNow;
                 }
                 await _context.SaveChangesAsync();
             }
--- Helpers/AutoMapperProfiles.cs
+++ Helpers/AutoMapperProfiles.cs
@@ -27,7 +27,8 @@ namespace DatingApp.Helpers
                 .ForMember(dest=>dest.SenderPhotoUrl,opt=>opt.MapFrom(
                     src=>src.Sender.Photos.FirstOrDefault(x=>x.IsMain).Url))
                 .ForMember(dest => dest.RecipientPhotoUrl, opt => opt.MapFrom(
-                    src => src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url)); ;
+                    src => src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
+            CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
         }
     }
 }
